4c62b9838a6c0f0fe409d63255116857d298bc0e
kang.yang <Kang.Yang@b-and-qchina.com>
2019-05-21 09:15:27 +0800

    数据结构+模块

---
+++ src/grammar2/mod/Support.py
@@ -0,0 +1,15 @@
+def amount(price, discount):
+    newPrice = ('{:.2f}'.format(price * discount / 10))
+    return newPrice
+
+
+def amount2(price):
+    if price > 88:
+        newPrice = ('{:.2f}'.format(price * 0.9))
+    elif price > 188:
+        newPrice = ('{:.2f}'.format(price * 0.8))
+    elif price > 288:
+        newPrice = ('{:.2f}'.format(price * 0.8))
+    else:
+        newPrice = price
+    return newPrice
